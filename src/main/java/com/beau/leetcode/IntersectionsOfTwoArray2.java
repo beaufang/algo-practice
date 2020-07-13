60f3d6e3b7401d71b436066e78ccedfd9f6ffac8
beaufang <beaufang@126.com>
2020-07-13 19:36:38 +0800

    leetcode 数组的交集

---
+++ src/main/java/com/beau/leetcode/IntersectionsOfTwoArray2.java
@@ -0,0 +1,62 @@
+package com.beau.leetcode;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+ leetcode 350：https://leetcode-cn.com/problems/intersection-of-two-arrays-ii/
+ */
+public class IntersectionsOfTwoArray2 {
+
+
+    // hashMap 法
+    public int[] intersect(int[] nums1, int[] nums2) {
+        // 为了节省空间，选取小的数组存储
+        if (nums1.length > nums2.length) {
+            return intersect(nums2, nums1);
+        }
+        Map<Integer, Integer> memo = new HashMap<>();
+        for (int n : nums1) {
+            int count = memo.getOrDefault(n, 0) + 1;
+            memo.put(n, count);
+        }
+        int[] res = new int[nums1.length];
+        int index = 0;
+        for (int n : nums2) {
+            int count = memo.getOrDefault(n, 0);
+            if (count > 0) {
+                res[index++] = n;
+                count--;
+                // 及时释放空间
+                if (count > 0) {
+                    memo.put(n, count);
+                } else {
+                    memo.remove(n);
+                }
+            }
+        }
+        return Arrays.copyOfRange(res, 0, index);
+    }
+
+    // 排序之后，双指针法
+    public int[] intersect2(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int i = 0, j = 0;
+        int[] res = new int[Math.min(nums1.length, nums2.length)];
+        int index = 0;
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] < nums2[j]) {
+                i++;
+            }else if (nums1[i] > nums2[j]) {
+                j++;
+            } else {
+                res[index++] = nums1[i];
+                i++;
+                j++;
+            }
+        }
+        return Arrays.copyOfRange(res, 0, index);
+    }
+}
