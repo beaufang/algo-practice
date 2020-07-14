91c1e1ed752e44fc427ca1b895f29d6b2d605858
beaufang <beaufang@126.com>
2020-07-14 17:45:52 +0800

    84 最大矩形面积

--- src/main/java/com/beau/Solution.java
+++ src/main/java/com/beau/Solution.java
@@ -1,26 +1,6 @@
 package com.beau;
 
-import java.util.Arrays;
-
 public class Solution {
-    // 排序之后，双指针法
-    public int[] intersect(int[] nums1, int[] nums2) {
-        Arrays.sort(nums1);
-        Arrays.sort(nums2);
-        int i = 0, j = 0;
-        int[] res = new int[Math.min(nums1.length, nums2.length)];
-        int index = 0;
-        while (i < nums1.length && j < nums2.length) {
-            if (nums1[i] < nums2[j]) {
-                i++;
-            }else if (nums1[i] > nums2[j]) {
-                j++;
-            } else {
-                res[index++] = nums1[i];
-                i++;
-                j++;
-            }
-        }
-        return Arrays.copyOfRange(res, 0, index);
-    }
+
+
 }
---
+++ src/main/java/com/beau/leetcode/LargestRectangleInHistogram.java
@@ -0,0 +1,42 @@
+package com.beau.leetcode;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+// 84
+public class LargestRectangleInHistogram {
+
+    // 单调栈+哨兵
+    @SuppressWarnings("all")
+    public int largestRectangleArea(int[] heights) {
+        int len = heights.length;
+        if (len == 0) {
+            return 0;
+        }
+        if (len == 1) {
+            return heights[0];
+        }
+        int[] newHeights = new int[len + 2];
+        System.arraycopy(heights, 0, newHeights, 1, len);
+        // 设置哨兵, 数组首位添加 0
+        newHeights[0] = 0;
+        newHeights[len + 1] = 0;
+        Deque<Integer> stack = new ArrayDeque<>();
+        stack.addLast(0);
+        int maxArea = 0;
+        for (int i = 1; i < newHeights.length; i++) {
+            while (newHeights[i] < newHeights[stack.peekLast()]) {
+                /* 注意这里计算高度和宽度：
+                    计算高度时，使用的是 poll 方法，让当前柱子出栈
+                    计算宽度时，使用的时 peek 方法，取出的是当前出栈柱子左侧最近的小于当前柱子高度的柱子的索引
+                */
+                int curHeight = newHeights[stack.pollLast()];
+                int width = i - stack.peekLast() - 1;
+                maxArea = Math.max(maxArea, width * curHeight);
+            }
+            // 别忘了把新元素入栈
+            stack.addLast(i);
+        }
+        return maxArea;
+    }
+}
