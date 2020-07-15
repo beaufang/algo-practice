439b904fda2c9de8dfcc5608e8f538ec80483594
beaufang <beaufang@126.com>
2020-07-15 23:22:11 +0800

    PlusOne

---
+++ src/main/java/com/beau/leetcode/PlusOne.java
@@ -0,0 +1,18 @@
+package com.beau.leetcode;
+
+// 66
+public class PlusOne {
+
+    public int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            digits[i] = digits[i] + 1;
+            digits[i] = digits[i] % 10;
+            // 不需要进位
+            if (digits[i] != 0) return digits;
+        }
+        // 处理 999 的情况
+        digits = new int[digits.length + 1 ]; // 数组默认初始化为 0
+        digits[0] = 1;
+        return digits;
+    }
+}
---
+++ src/main/java/com/beau/leetcode/TwoSum.java
@@ -0,0 +1,20 @@
+package com.beau.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+// 1
+public class TwoSum {
+
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int goal = target - nums[i];
+            if (map.containsKey(goal)) {
+                return new int[]{i, map.get(goal)};
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("no such solution");
+    }
+}
