746c9001b5df2eebd48e1782a767cf5a8948ac30
beaufang <beaufang@126.com>
2020-07-27 07:29:52 +0800

    柠檬水找零

---
+++ src/main/java/com/beau/leetcode/week3/LemonadeChange.java
@@ -0,0 +1,34 @@
+package com.beau.leetcode.week3;
+
+/**
+ * @author BeauFang
+ * Date: 2020/7/27
+ * 860 https://leetcode-cn.com/problems/lemonade-change/
+ */
+public class LemonadeChange {
+
+    public boolean lemonadeChange(int[] bills) {
+        int five = 0, ten = 0;
+        for (int bill : bills) {
+            if (bill == 5) {
+                five++;
+            } else if (bill == 10) {
+                if (five == 0) {
+                    return false;
+                }
+                five--;
+                ten++;
+            } else {
+                if (ten > 0 && five > 0) {
+                    ten--;
+                    five--;
+                } else if (five >= 3) {
+                    five -= 3;
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
