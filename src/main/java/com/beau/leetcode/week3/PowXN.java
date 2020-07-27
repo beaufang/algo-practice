6da5f1424812e5ba641da41c2641d1fe161f7e00
beaufang <beaufang@126.com>
2020-07-28 07:57:00 +0800

    Pow(x,n)

---
+++ src/main/java/com/beau/leetcode/week3/PowXN.java
@@ -0,0 +1,27 @@
+package com.beau.leetcode.week3;
+
+/**
+ * @author BeauFang
+ * Date: 2020/7/28
+ * 50 https://leetcode-cn.com/problems/powx-n/
+ */
+public class PowXN {
+
+    public double myPow(double x, int n) {
+        // 防止 -n 越界
+        long N = n;
+        return N < 0 ? 1/myPowHelper(x, -N) : myPowHelper(x, N);
+    }
+
+    private double myPowHelper(double x, long n) {
+        if (x == 1 || n == 0) {
+            return 1;
+        }
+        double half = myPowHelper(x, n/2);
+        if (n % 2 == 0) {
+            return half * half;
+        } else {
+            return x * half * half;
+        }
+    }
+}
