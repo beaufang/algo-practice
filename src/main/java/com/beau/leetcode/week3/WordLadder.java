5af7c87f9da77b1a66187656a23826d96bdd9603
beaufang <beaufang@126.com>
2020-07-28 21:30:12 +0800

    单词接龙

---
+++ src/main/java/com/beau/leetcode/week3/WordLadder.java
@@ -0,0 +1,54 @@
+package com.beau.leetcode.week3;
+
+import java.util.*;
+
+/**
+ * @author BeauFang
+ * Date: 2020/7/28
+ * 127 https://leetcode-cn.com/problems/word-ladder/
+ */
+public class WordLadder {
+
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        // 将 wordList 转换成 hashSet 方便判断
+        Set<String> set = new HashSet<>(wordList);
+        set.remove(beginWord);
+        // 广度优先搜索
+        Queue<String> queue = new LinkedList<>();
+        Set<String> visited = new HashSet<>();
+        queue.add(beginWord);
+        visited.add(beginWord);
+        int step = 1;
+        while (!queue.isEmpty()) {
+            int level = queue.size();
+            for (int l = 0; l < level; l++) {
+                String str = queue.poll();
+                // 对 str 的每个字符做 a-z 的变换，查找单词列表是否存在
+                char[] chars = str.toCharArray();
+                for (int i = 0; i < chars.length; i++) {
+                    char originalChar = chars[i];
+                    for (char j = 'a'; j <= 'z'; j++) {
+                        if (j == originalChar) {
+                            continue;
+                        }
+                        chars[i] = j;
+                        String newString = new String(chars);
+                        if (set.contains(newString)) {
+                            if (newString.equals(endWord)) {
+                                return step + 1;
+                            }
+                            if (!visited.contains(newString)) {
+                                queue.add(newString);
+                                visited.add(newString);
+                            }
+                        }
+                        // 恢复
+                        chars[i] = originalChar;
+                    }
+                }
+            }
+            step++;
+        }
+        return 0;
+    }
+}
