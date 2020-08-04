2b7e208c02c89e89e733de022bd3ceaa32953b65
beaufang <beaufang@126.com>
2020-08-04 21:29:05 +0800

    任务调度器

---
+++ src/main/java/com/beau/leetcode/week4/TaskScheduler.java
@@ -0,0 +1,26 @@
+package com.beau.leetcode.week4;
+
+import java.util.Arrays;
+
+/**
+ * @author BeauFang
+ * Date: 2020/8/4
+ * 621 https://leetcode-cn.com/problems/task-scheduler/
+ */
+public class TaskScheduler {
+
+    public int leastInterval(char[] tasks, int n) {
+        int[] map = new int[26];
+        for (char c : tasks) {
+            map[c - 'A']++;
+        }
+        Arrays.sort(map);
+        int maxCount = map[25] - 1;
+        int idle = n * maxCount;
+        for (int i = 24; i >= 0 && map[i] > 0; i--) {
+            idle -= Math.min(map[i], maxCount);
+        }
+        return idle > 0 ? tasks.length + idle : tasks.length;
+    }
+
+}
